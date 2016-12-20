c617756fba819168032f9c686bfbb9dc11739ab9
abakkour <akram.bakkour@gmail.com>
2016-12-20 16:34:36 -0500

    classification with pymvpa's LinearCSVMC on yeti. Crossvalidation using Hippocampus mask and searchlight on whole brain

--- run_classify_sub.py
+++ run_classify_sub.py
@@ -4,13 +4,15 @@ import mvpa2, sklearn, numpy, os, nibabel, sys
 from mvpa2.suite import *
 
 #where the data live
+data_path="/vega/psych/users/ab4096/MDMRT_scan/"
 #data_path="/data/akram/MDMRT_scan/"
-data_path="/Volumes/hypatia/akram/MDMRT_scan/"
+#data_path="/Volumes/hypatia/akram/MDMRT_scan/"
 
+wb_mask_fname="/vega/psych/app/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz"
 #wb_mask_fname="/usr/share/fsl/5.0/data/standard/MNI152_T1_2mm_brain_mask.nii.gz"
-wb_mask_fname="/usr/local/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz"
+#wb_mask_fname="/usr/local/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz"
 
-sub = "sub002"
+sub = str(sys.argv[1])  #"sub002"
 
 #Paths for the nifti images    
 beta_t1_fname = os.path.join(data_path,sub, 'model/model100/task001_LSS_std.nii.gz')
@@ -32,6 +34,7 @@ zscore(ds2,chunks_attr='chunks',dtype='float32')
 
 #specify classifier
 clf = LinearCSVMC()
+#clf=sklearn.svm.SVC(kernel='linear',probability=True)
 
 #set up three-fold cross validation
 cvte1 = CrossValidation(clf, NFoldPartitioner(), errorfx=lambda p, t: np.mean(p == t),enable_ca=['stats'])
@@ -39,7 +42,11 @@ cvte2 = CrossValidation(clf, NFoldPartitioner(), errorfx=lambda p, t: np.mean(p 
 
 #run cross validation on task1+2 whole-brain data
 t1cv_results = cvte1(ds1)
+numpy.savetxt(os.path.join(data_path,sub,"model/model100/task001_std_cv.txt"),t1cv_results.samples,delimiter=",")
 t2cv_results = cvte2(ds2)
+numpy.savetxt(os.path.join(data_path,sub,"model/model100/task002_std_cv.txt"),t2cv_results.samples,delimiter=",")
+
+#print cvte1.ca.stats.as_string(description=True)
 
 
 ##searchlight
@@ -58,3 +65,7 @@ sl = sphere_searchlight(cvte, radius=6, postproc=mean_sample())
 t1_sl_results = sl(ds1)
 t1_niftiresults = map2nifti(t1_sl_results, imghdr=ds1.a.imghdr)
 niftiresults.to_filename(os.path.join(data_path,sub,'model/model100/searchlight_results_2mm_task001_3class.nii.gz'))
+
+t2_sl_results = sl(ds2)
+t2_niftiresults = map2nifti(t2_sl_results, imghdr=ds2.a.imghdr)
+niftiresults.to_filename(os.path.join(data_path,sub,'model/model100/searchlight_results_2mm_task002_3class.nii.gz'))
